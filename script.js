1bcfb61dc1c06af595f3d9460076c5e6574d48cd
Cameron Chandler <webstormcam@gmail.com>
2021-12-21 21:15:22 -0500

    Figured out how to switch the ham lol

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 
 function changeImage(){
     let hampic = document.getElementById('ham')
-    if(hampic.src === 'imgs/icon-close.svg'){
+    if(hampic.getAttribute('src') === 'imgs/icon-close.svg'){
         hampic.src = 'imgs/icon-hamburger.svg'
-    } else{
+    } else {
          hampic.src = 'imgs/icon-close.svg'
-       hampic.alt ='catman'
      }
     console.log('click')
 }
