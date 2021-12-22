e66e51e4c11dd23f1f16cb29af4d42c6d2e93d2f
Cameron Chandler <webstormcam@gmail.com>
2021-12-21 21:11:24 -0500

    HONESTLY NO IDEA LOL

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 
 function changeImage(){
-    let hampic = document.getElementById('ham');
+    let hampic = document.getElementById('ham')
     if(hampic.src === 'imgs/icon-close.svg'){
-        hampic.src ='imgs/icon-hamburger.svg'
+        hampic.src = 'imgs/icon-hamburger.svg'
     } else{
-        hampic.src ='imgs/icon-close.svg'
-    }
+         hampic.src = 'imgs/icon-close.svg'
+       hampic.alt ='catman'
+     }
     console.log('click')
 }
 
