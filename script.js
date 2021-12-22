8c76578e9ca31ad01f1b82a0041747ac5aca91c9
Cameron Chandler <webstormcam@gmail.com>
2021-12-21 20:18:32 -0500

    Started implementing the JS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let ham = document.getElementById('ham')
+
+function changeImage(){
+    if(ham.src ==='imgs/icon-close.svg'){
+        ham.src = 'imgs/icon-hamburger.svg'
+    } 
+    else{
+        ham.src ='imgs/icon-close.svg'
+    }
+    console.log('click')
+}
