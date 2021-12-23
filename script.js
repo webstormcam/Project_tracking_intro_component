1360e2434c0b10d1c26f871ca09c10691b5675b2
Cameron Chandler <webstormcam@gmail.com>
2021-12-23 07:23:19 -0500

    Got the box to appear and disappear

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
 function changeImage(){
     let hampic = document.getElementById('ham')
+    let drop = document.getElementById('dropdown');
     if(hampic.getAttribute('src') === 'imgs/icon-close.svg'){
         hampic.src = 'imgs/icon-hamburger.svg'
+        drop.style.display ='none'
     } else {
          hampic.src = 'imgs/icon-close.svg'
+         drop.style.display ='flex'
      }
     console.log('click')
 }
